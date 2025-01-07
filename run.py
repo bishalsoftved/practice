09ab1dc32dc6d810883718af51c456fb3c8dfdd8
bishal2222 <bishal2222>
2025-01-07 10:38:56 +0545

    used flask to develop python web app

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-#!/usr/bin/env python3
+from flask import Flask
 
-print("Hello world")
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello from the docker world"
+
+if __name__ == "__main__":
+	app.run(debug=True, host="0.0.0.0", port=5000)
